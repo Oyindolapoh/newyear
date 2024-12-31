b049205bb94333476b377ec6fb3c62e928442517
Agbaje Oyindolapo Adefemi <yinzee98@gmail.com>
2024-12-31 17:05:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const h2 = document.querySelector('h2');
+const blocks = document.querySelector('.blocks');
+
+h2.addEventListener('click',
+    ()=>{
+        blocks.classList.toggle
+        ('active');
+h2.classList.toggle
+('active');
+},
+);
+
+
